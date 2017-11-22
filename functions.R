9c9b892406ec1714f11807d15073de10bd061cbb
mindymallory <mallorym@illinois.edu>
2017-11-21 21:02:23 -0600

    ,m

--- functions.R
+++ functions.R
@@ -25,42 +25,52 @@ month_symbol <- function(monthint){
 }
 
 
-get_nearby_corn <- function(MonthCode){
+get_nearby_corn <- function(MonthCode, Year){
   codes <- c('F', 'G', 'H', 'J', 'K', 'M', 'N', 'Q', 'U', 'V', 'X', 'Z', 'F', 'G', 'H')
   corn_codes <- c('H', 'K','N', 'U', 'Z', 'H')
   
+  if (is.na(match(MonthCode,corn_codes)) == FALSE) II <- match(MonthCode,codes)
   while(is.na(match(MonthCode,corn_codes)) == TRUE){
-    MonthCode <- match(MonthCode,codes)+1
-    MonthCode <- codes[MonthCode]
+    II <- match(MonthCode,codes)+1
+    MonthCode <- codes[II]
   }
   
-  return(MonthCode)
+  if (II > 13) Year <- Year + 1
+  
+  Cm <- list(MonthCode, Year)
+  return(Cm)
+  
 }
 
-corn_forward8 <- function(MonthCode, Year){
+# 6 contracts corn and wheat is just about 1 year. 8 contracts soybeans is just about 1 year
+corn_forward6 <- function(MonthCode, Year){
   c <- c('H', 'K','N', 'U', 'Z', 'H', 'K','N', 'U', 'Z', 'H', 'K','N', 'U', 'Z')
   y      <-  c(rep(Year, each = 5), rep(Year+1, each = 5), rep(Year +2, each = 5))
   corn_codes <- cbind(c, y)
   contract <- match(MonthCode, corn_codes)
-  contract8 <- corn_codes[contract:(contract+7),]
-  return(contract8)
+  contract6 <- corn_codes[contract:(contract+5),]
+  return(contract6)
 }
 
-get_nearby_soy <- function(MonthCode){
-  codes <- c('F', 'G', 'H', 'J', 'K', 'M', 'N', 'Q', 'U', 'V', 'X', 'Z', 'F')
+get_nearby_soy <- function(MonthCode, Year){
+  codes <- c('F', 'G', 'H', 'J', 'K', 'M', 'N', 'Q', 'U', 'V', 'X', 'Z', 'F', 'G', 'H')
   soy_codes <- c('F', 'H', 'K','N', 'Q', 'U', 'X')
   
+  if (is.na(match(MonthCode,soy_codes)) == FALSE) II <- match(MonthCode,codes)
   while(is.na(match(MonthCode,soy_codes)) == TRUE){
-    MonthCode <- match(MonthCode,codes)+1
-    MonthCode <- codes[MonthCode]
+    II <- match(MonthCode,codes)+1
+    MonthCode <- codes[II]
   }
   
-  return(MonthCode)
+  if (II > 12) Year <- Year + 1
+  
+  Sm <- list(MonthCode, Year)
+  return(Sm)
 }
 
 soy_forward8 <- function(MonthCode, Year){
   s <- c('F', 'H', 'K','N', 'Q', 'U', 'X', 'F', 'H', 'K','N', 'Q', 'U', 'X', 'F', 'H', 'K','N', 'Q', 'U', 'X')
-  y         <- c(rep(Year, each = 7), rep(Year+1, each = 7), rep(Year +2, each = 7))
+  y         <- c(rep(Year, each = 7), rep(Year+1, each = 7), rep(Year +2, each = 7), rep(Year + 3, each = 7))
   corn_codes <- cbind(s, y)
   contract <- match(MonthCode, corn_codes)
   contract8 <- corn_codes[contract:(contract+7),]
